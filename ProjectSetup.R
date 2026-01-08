0940a7e2d608c2e092bdf49c97c6e56b506a52cb
Michael Akpabey <micakpabey@gmail.com>
2026-01-07 19:14:02 -0500

    Project initiation/setup

---
+++ ProjectSetup.R
@@ -0,0 +1,41 @@
+# Installing necessary packages 
+
+install.packages(c(
+  "golem", "usethis", "devtools", "testthat", "lintr",
+  "config", "here", "DT", "bslib", "plotly", "renv"
+))
+
+
+#Creating golem project
+
+library(golem)
+create_golem("BulkQC")
+
+# Intial scaffold
+library(golem)
+use_recommended_tests()
+use_recommended_deps()
+use_utils_ui()
+
+# Initializing modules 
+golem::add_module(name = "upload")
+golem::add_module(name = "filters")
+golem::add_module(name = "qc_overview")
+golem::add_module(name = "pca")
+golem::add_module(name = "export")
+
+usethis::use_r("qc_validate")
+usethis::use_r("qc_metrics")
+usethis::use_r("qc_dimred")
+usethis::use_r("qc_outliers")
+
+
+dir.create("inst/extdata", recursive = TRUE, showWarnings = FALSE)
+dir.create("inst/report_templates", recursive = TRUE, showWarnings = FALSE)
+dir.create("inst/app/www", recursive = TRUE, showWarnings = FALSE)
+
+
+golem::run_dev()
+
+renv::init()
+renv::snapshot()
